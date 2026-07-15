bd3447a5929b7ad37484b2f077c5231cc5e04b8d
nadeem5512 <nadeem5512@gmail.com>
2026-07-15 17:01:26 +0530

    Initial Java Maven project

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Jenkins CI/CD Demo");
+        System.out.println("Built by Jenkins");
+        System.out.println("=================================");
+
+    }
+}
